3782eb4ef8ee9b39d42498e5a8e141c91b235377
Riley Kuhlman <rilstarssun@gmail.com>
2022-12-12 19:31:12 -0600

    Move away from calibrating in HDX711 code.

--- src/calibration/test_accuracy.h
+++ src/calibration/test_accuracy.h
@@ -31,10 +31,6 @@ float data2[STORABLE_VALUES];
 float data3[STORABLE_VALUES];
 int last_written_index = 0;
 
-float recordedCalibrationFactor1;
-float recordedCalibrationFactor2;
-float recordedCalibrationFactor3;
-
 float cal1 = 0.06654563711;
 float cal2 = 0.07054315246;
 float cal3 = 0.06976268166;
@@ -45,8 +41,6 @@ void setup() {
     delay(1000);
 
     Serial.println();
-    Serial.println("Starting...");
-
     Serial.println("Initializing load cells.");
 
     LoadCell1.begin();
@@ -55,23 +49,22 @@ void setup() {
 
     
     // EEPROM.get(LOAD_CELL_1_CAL, recordedCalibrationFactor1);
-    recordedCalibrationFactor1 = 1.0f;
-    Serial.print("(1): Using calibration factor of ");
-    Serial.println(recordedCalibrationFactor1);
-    LoadCell1.setCalFactor(recordedCalibrationFactor1);
+    Serial.print("(1): Cal: ");
+    Serial.println(cal1);
 
     // EEPROM.get(LOAD_CELL_2_CAL, recordedCalibrationFactor2);
-    recordedCalibrationFactor2 = 1.0f;
-    Serial.print("(2): Using calibration factor of ");
-    Serial.println(recordedCalibrationFactor2);
-    LoadCell2.setCalFactor(recordedCalibrationFactor2);
+    Serial.print("(2): Cal ");
+    Serial.println(cal2);
+    
 
     // EEPROM.get(LOAD_CELL_3_CAL, recordedCalibrationFactor3);
-    recordedCalibrationFactor3 = 1.0f;
-    Serial.print("(3): Using calibration factor of ");
-    Serial.println(recordedCalibrationFactor3);
-    LoadCell3.setCalFactor(recordedCalibrationFactor3);
+    Serial.print("(3): Cal ");
+    Serial.println(cal3);
     
+    LoadCell2.setCalFactor(1);
+    LoadCell3.setCalFactor(1);
+    LoadCell1.setCalFactor(1);
+
     LoadCell1.tareNoDelay();
     LoadCell2.tareNoDelay();
     LoadCell3.tareNoDelay();
@@ -99,11 +92,11 @@ void handleNewData() {
         Serial.println(total);
         
         Serial.print("Load Cell 1: ");
-        Serial.print(new_measurement1 / recordedCalibrationFactor1);
+        Serial.print(new_measurement1);
         Serial.print("; Load Cell 2: ");
-        Serial.print(new_measurement2 / recordedCalibrationFactor2);
+        Serial.print(new_measurement2);
         Serial.print("; Load Cell 3: ");
-        Serial.println(new_measurement3 / recordedCalibrationFactor3);
+        Serial.println(new_measurement3);
 
         last_printed = millis();
     }
@@ -140,17 +133,17 @@ void loop() {
         // For some reason getData outputs an int. Bruh.
         new_measurement1 = LoadCell1.getData();
         handleNewData();
-    };
+    }
 
     if (LoadCell2.update()) {
         new_measurement2 = LoadCell2.getData();
         handleNewData();
-    };
+    }
 
     if (LoadCell3.update()) {
         new_measurement3 = LoadCell3.getData();
         handleNewData();
-    };
+    }
 
 
     if (Serial.available()) {
@@ -172,9 +165,9 @@ void loop() {
             recording_value = false;
 
             Serial.println("Average values are: ");
-            Serial.println(get_mean(data1) / recordedCalibrationFactor1);
-            Serial.println(get_mean(data2) / recordedCalibrationFactor2);
-            Serial.println(get_mean(data3) / recordedCalibrationFactor3);
+            Serial.println(get_mean(data1));
+            Serial.println(get_mean(data2));
+            Serial.println(get_mean(data3));
             Serial.print("Total: ");
             
             Serial.println(cal1 * get_mean(data1) + cal2 * get_mean(data2) + cal3 * get_mean(data3));
